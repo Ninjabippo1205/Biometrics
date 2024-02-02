9d07971b0c7570ad66ab028e3d7ffa1403f5cfd6
Filippo Guerra <filippoguerra2001@gmail.com>
2024-02-02 18:19:40 +0100

    Added gitignored, followed Ludovica Gaurfi in normalization and used multiprocessing to speed up calculcations

--- iris_identification.py
+++ iris_identification.py
@@ -1,68 +1,63 @@
 import cv2
 import iris_recognition as IR
 from scipy.spatial import distance as scipydistance
+import multiprocessing
+                
+def hammingdistance(probe, galleryimage, queue, gallerysubject):
+	galleryimage = IR.getTemplate(galleryimage).ravel()
+	queue.put((scipydistance.hamming(probe, galleryimage), gallerysubject))
+	return 0 #correct exit code
 
-
-import numpy as np
-
-def pad_vector(vector, target_length):
-    padding_length = target_length - len(vector)
-    if padding_length > 0:
-        vector = np.pad(vector, (0, padding_length), 'constant')
-    return vector
-
-
-
-def hammingdistance(img1, img2):
-	template1 = IR.getTemplate(img1)
-	template2 = IR.getTemplate(img2)
-
-	if(len(template1)>len(template2)):
-		template2=pad_vector(template2,len(template1))
-	else:
-		template1=pad_vector(template1,len(template2))
-
-
-	#w1, h1 = template1.shape
-	#w2, h2 = template2.shape
-
-	#w = int((w1+w2)/2)
-	#h = int((h1+h2)/2)
-	
-	#template1 = cv2.resize(template1, (w, h))
-	#template2 = cv2.resize(template2, (w, h))
-
-	#cv2.imshow("feature1", template1) 
-	#cv2.imshow("feature2", template2) 
-	#key = cv2.waitKey(3000)
-	#if(key==27 or key == 1048603):
-	#	return template1
-	return scipydistance.hamming(template1.ravel(), template2.ravel())
-
-
-def image_matching(path, test_subject, probe, gallery, gallery_subjects, threshold):
+def image_matching(path, test_subject, probe, gallery, gallery_subjects, threshold, subprocess_count=7):
 	minDistance = float("inf")
 	matched = ''
 	matched_list = {}
-		
+
 	#for image_path in probe:
 	image_path = probe
 	probeimage = cv2.imread(f"{path}/{test_subject}/{image_path}")
-	
-	for gallery_subject in range(len(gallery)):
-		for test_path in gallery[gallery_subject]:
+	probeimage = IR.getTemplate(probeimage).ravel()
 
+	jobs = []
+	distances = []
+	queue = multiprocessing.Queue()
+
+	for gallery_subject in range(len(gallery)):
+		distances.append(list())
+		for i in range(len(gallery[gallery_subject])):
+			test_path = gallery[gallery_subject][i]
 			if(image_path == test_path): continue
 
-			galleryimage = cv2.imread(f"{path}/{gallery_subjects[gallery_subject]}/{test_path}")
-			
-			d = hammingdistance(probeimage, galleryimage)
-			if(d < minDistance):
-				minDistance = d
+			galleryimage = cv2.imread(f'{path}/{gallery_subjects[gallery_subject]}/{test_path}')
+			p = multiprocessing.Process(target=hammingdistance, args=(probeimage, galleryimage, queue, gallery_subject, ))
+			jobs.append(p)
+			p.start()
+
+			if(len(jobs) > subprocess_count):
+				for j in jobs: j.join()
+				jobs.clear()
+
+				while not queue.empty():
+					distance, gallerysub = queue.get()
+					distances[gallerysub].append(distance)
+	
+	for p in jobs: p.join()
+	while not queue.empty():
+		distance, gallerysub = queue.get()
+		distances[gallery_subject].append(distance)
+
+	queue.close()
+	queue.join_thread()
+	
+	for gallery_subject in range(len(distances)):
+		for dist in distances[gallery_subject]:
+
+			if(dist < minDistance):
+				minDistance = dist
 				matched = gallery_subjects[gallery_subject]
 
-			if(d < threshold):
-				matched_list[gallery_subjects[gallery_subject]] = d
+			if(dist < threshold):
+				matched_list[gallery_subjects[gallery_subject]] = dist
 
 	matched_list = dict(sorted(matched_list.items(), key=lambda item: item[1]))	
 	return [matched, minDistance, matched_list]
--- iris_recognition.py
+++ iris_recognition.py
@@ -115,28 +115,84 @@ def eyelashes_mask(eye_image):
 	return closed_mask
 
 def normalizeWithPolarCoordinates(image, center, pupil_radius, iris_radius):
-    img = image.copy()
-    # Crea una griglia di angoli e raggi
-    angles = np.linspace(-np.pi, np.pi, int(2*np.pi*iris_radius))  # larghezza proporzionale alla circonferenza dell'iride
-    radii = np.linspace(pupil_radius, iris_radius, int(iris_radius-pupil_radius))  # altezza proporzionale alla differenza dei raggi
-    grid_angle, grid_radius = np.meshgrid(angles, radii)
-    # Mappa gli angoli e i raggi alla x e alla y
-    map_x = grid_radius * np.cos(grid_angle) + center[0]
-    map_y = grid_radius * np.sin(grid_angle) + center[1]
-    # Interpola i pixel dell'immagine originale sulla griglia
-    unwrapped_img = cv2.remap(img, map_x.astype('float32'), map_y.astype('float32'), cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT, borderValue=0)
-    # Converte l'immagine srotolata in uint8
-    unwrapped_img = cv2.normalize(unwrapped_img, None, 0, 255, cv2.NORM_MINMAX).astype('uint8')
-    return unwrapped_img
+	img = image.copy()
+	# Crea una griglia di angoli e raggi
+	radii = np.linspace(pupil_radius, iris_radius, 100)  	# altezza proporzionale alla differenza dei raggi
+	angles = np.arange(0, 2*np.pi, 2*np.pi/360)						# larghezza proporzionale alla circonferenza dell'iride
+	
+	unwrapped_img = np.zeros((radii.size, angles.size))
+
+	for i in range(radii.size):
+		for j in range(angles.size):
+			unwrapped_img[i, j] = img[int(center[1] + radii[i]*np.sin(angles[j])), int(center[0] + radii[i]*np.cos(angles[j]))]
+	return unwrapped_img
 
 def eyelid_mask(img):
 	h, w = img.shape
 	new_bigger_img = np.zeros((h+300, w+300), dtype=np.uint8)
 	new_bigger_img[0:h, 0:w] = img
-	#cv2.imshow("new_bigger_img", new_bigger_img)
 	return img
 
+def eyelid_mask_after_normalization(img):
+	kernel = np.ones((8,8),np.uint8)
+	img_eroded = cv2.erode(img, kernel, iterations = 1)
+	contorni, hierarchy = cv2.findContours(img_eroded, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+	contorni = [contour for contour, h in zip(contorni, hierarchy[0]) if h[3] == -1]
+	max_area = max(cv2.contourArea(contorno) for contorno in contorni)
+	new = np.zeros_like(img)
+	for contorno in contorni:
+		area_contorno = cv2.contourArea(contorno)
+		if area_contorno < 1000:
+			continue
+		if area_contorno < max_area/6:
+			continue
+		cv2.drawContours(new, [contorno], -1, 255, -1)
+	return new
 
+def eyelid_mask_before_normalization(img, pupil_center, pupil_radius, iris_center, iris_radius):
+	blurred = cv2.GaussianBlur(img, (15, 15), 0) #21,21
+
+	mask = np.zeros_like(img)
+	cv2.circle(mask, iris_center, iris_radius, (255,255,255), -1)
+	cv2.circle(mask, pupil_center, pupil_radius, (0,0,0), -1)
+	mean = cv2.mean(blurred, mask=mask)[0]
+
+	if mean > 120:
+		thresh = cv2.threshold(blurred, 0.43*mean, 255, cv2.THRESH_TOZERO)[1]
+		thresh = cv2.threshold(thresh, 1*mean, 255, cv2.THRESH_TOZERO_INV)[1]
+	elif mean > 100:
+		thresh = cv2.threshold(blurred, 0.7*mean, 255, cv2.THRESH_TOZERO)[1]
+		thresh = cv2.threshold(thresh, 1.35*mean, 255, cv2.THRESH_TOZERO_INV)[1]
+	elif mean > 80:
+		thresh = cv2.threshold(blurred, 0.66*mean, 255, cv2.THRESH_TOZERO)[1]
+		thresh = cv2.threshold(thresh, 1.36*mean, 255, cv2.THRESH_TOZERO_INV)[1]
+	else:
+		thresh = cv2.threshold(blurred, 0.5*mean, 255, cv2.THRESH_TOZERO)[1]
+		thresh = cv2.threshold(thresh, 1.4*mean, 255, cv2.THRESH_TOZERO_INV)[1]
+	
+	mask = cv2.threshold(thresh, 0, 255, cv2.THRESH_BINARY)[1]
+	
+	contorni, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+	contorni = [contour for contour, h in zip(contorni, hierarchy[0]) if h[3] == -1]
+	new = np.zeros_like(img)
+
+	max_area = max(cv2.contourArea(contorno) for contorno in contorni)
+	c = 0
+	avg_color_contour = []
+	for contorno in contorni:
+		if cv2.contourArea(contorno) < max_area/4:
+			continue
+		contour_mask = np.zeros_like(img)
+		cv2.drawContours(contour_mask, [contorno], -1, 255, 2)
+		cv2.drawContours(new, [contorno], -1, 255, 2)
+		c = c+1
+		avg_color_contour.append([cv2.mean(img, mask=contour_mask)[0], contorno])
+
+	contorno = sorted(avg_color_contour, key=lambda x: x[0], reverse=True)[0][1]
+	res = np.zeros_like(img)
+	cv2.drawContours(res, [contorno], -1, 255, -1)
+	ris = cv2.bitwise_and(res, mask)
+	return ris
 
 def getTemplate(path):
 	global verbose
@@ -149,32 +205,48 @@ def getTemplate(path):
 	min_radius = int(pupil_radius + (0.3*pupil_radius))
 	max_radius = int(pupil_radius + (0.2*pupil_radius) + 100)	
 	iris_center, iris_radius = getIris(preProcessed_image_for_iris, param1=30, param2=400, minDist=0.01, minRadius=min_radius, maxRadius=max_radius)
-				
+
 	lashes_mask = eyelashes_mask(frame.copy())
 	partial_iris = not_iris_mask(pupil_center, iris_center, pupil_radius, iris_radius, frame)
+			
+	p_iris = eyelid_mask_before_normalization(partial_iris, pupil_center, pupil_radius, iris_center, iris_radius)
+	partial_iris = cv2.bitwise_and(partial_iris, partial_iris, mask=p_iris)
+	iris_without_eyelid_and_eyelashes_before_normalization = cv2.bitwise_and(partial_iris, lashes_mask)
 
 	# Adding eyelashes mask to image and normalizing
-	normalized_iris = normalizeWithPolarCoordinates(cv2.bitwise_and(partial_iris, lashes_mask), iris_center, pupil_radius, iris_radius)
+	normalized_iris = normalizeWithPolarCoordinates(iris_without_eyelid_and_eyelashes_before_normalization, iris_center, pupil_radius, iris_radius)
 	final_iris = eyelid_mask(normalized_iris)
 	
-	if(verbose): cv2.imshow("normalized_iris", final_iris)
+	#eyelid_mask_ = eyelid_mask_after_normalization(normalized_iris)
+	#final_iris = cv2.bitwise_and(normalized_iris, normalized_iris, mask=eyelid_mask_)
 
-	width = final_iris.shape[0]
-	height = final_iris.shape[1]
+	filters = build_filters()
+	filitered_iris = process(final_iris, filters)
+	vector = binarize(filitered_iris)
+
+	return vector
+
+def process(img, filters):
+	accum = np.zeros_like(img)
+	for kern, params in filters:
+		fimg = cv2.filter2D(img, cv2.CV_8UC3, kern)
+		np.maximum(accum, fimg, accum)
+	return accum
+
+def binarize(img, threshold=128):
+	return (img > threshold).astype(np.uint8)
+
+def build_filters():
+	filters = []
+	ksize = 31
+	for theta in np.arange(0, np.pi, np.pi/16):
+		params = {'ksize' : (ksize, ksize), 'sigma' : 1.0, 'theta' : theta, 'lambd' : 15.0, 'gamma' : 0.02, 'psi' : 0, 'ktype' : cv2.CV_32F}
+		kern = cv2.getGaborKernel(**params)
+		kern /= 1.5 * kern.sum()
+		filters.append((kern, params))
 	
-	sigma = 6; theta = 0.8; lambda_ = 10; gamma = 2
-	gabor_kernel = cv2.getGaborKernel((width, height), sigma, theta, lambda_, gamma)
-	filtered_iris = cv2.filter2D(final_iris, cv2.CV_64F, gabor_kernel)
-	
-	cv2.imshow("feature2", filtered_iris) 
-	key = cv2.waitKey(3000)
-	if(key==27 or key == 1048603):
-		return gabor_kernel
-	vector = filtered_iris.flatten()
+	return filters
 
-	if(verbose): cv2.imshow("feature", filtered_iris)
-
-	return vector #filtered_iris
 
 def main(*, verb=False):
 	global verbose
@@ -199,7 +271,7 @@ def main(*, verb=False):
 	# Calculating False Acceptance, Good Rejection, Detect Indentification, Total Genuine and Total Impostor
 	#									(yes|no),				(no|no),						(yes|yes),						(), 								()  
 	FA = 0; GR = 0; TG =len(gallery_subjects); TI = len(gallery_subjects)
-	DI = np.zeros(len(gallery_subjects)-1)
+	DI = np.zeros(len(gallery_subjects*40)-1)
 
 	## image matching ##
 	for test_subject in d_keys:
