50665c80849f0f0b95515a8c7f927063513ffd2b
Filippo Guerra <filippoguerra2001@gmail.com>
2024-02-02 18:42:05 +0100

    Processes now work for a single subject. TODO: Let processes start as soon as one is finished

--- iris_identification.py
+++ iris_identification.py
@@ -3,9 +3,19 @@ import iris_recognition as IR
 from scipy.spatial import distance as scipydistance
 import multiprocessing
                 
-def hammingdistance(probe, galleryimage, queue, gallerysubject):
-	galleryimage = IR.getTemplate(galleryimage).ravel()
-	queue.put((scipydistance.hamming(probe, galleryimage), gallerysubject))
+def subject_hamming_distance(gallery, gallery_subjects, gallery_subject, path, image_path, probeimage, queue, ):
+	distances = []
+
+	for i in range(len(gallery[gallery_subject])):
+		test_path = gallery[gallery_subject][i]
+		if(image_path == test_path): continue
+
+		galleryimage = cv2.imread(f'{path}/{gallery_subjects[gallery_subject]}/{test_path}')
+		galleryimage = IR.getTemplate(galleryimage).ravel()
+
+		distances.append(scipydistance.hamming(probeimage, galleryimage))
+	
+	queue.put(distances)
 	return 0 #correct exit code
 
 def image_matching(path, test_subject, probe, gallery, gallery_subjects, threshold, subprocess_count=7):
@@ -13,42 +23,39 @@ def image_matching(path, test_subject, probe, gallery, gallery_subjects, thresho
 	matched = ''
 	matched_list = {}
 
-	#for image_path in probe:
-	image_path = probe
-	probeimage = cv2.imread(f"{path}/{test_subject}/{image_path}")
+	# Getting template for probe
+	probeimage = cv2.imread(f"{path}/{test_subject}/{probe}")
 	probeimage = IR.getTemplate(probeimage).ravel()
 
+	# Multiprocessing items
 	jobs = []
 	distances = []
 	queue = multiprocessing.Queue()
 
+	# Use a process for each subject
 	for gallery_subject in range(len(gallery)):
-		distances.append(list())
-		for i in range(len(gallery[gallery_subject])):
-			test_path = gallery[gallery_subject][i]
-			if(image_path == test_path): continue
+		p = multiprocessing.Process(target=subject_hamming_distance, args=(gallery, gallery_subjects, gallery_subject, path, probe, probeimage, queue, ))
+		jobs.append(p)
+		p.start()
 
-			galleryimage = cv2.imread(f'{path}/{gallery_subjects[gallery_subject]}/{test_path}')
-			p = multiprocessing.Process(target=hammingdistance, args=(probeimage, galleryimage, queue, gallery_subject, ))
-			jobs.append(p)
-			p.start()
+		if len(jobs) > subprocess_count:
+			for j in jobs: j.join()
+			jobs.clear()
 
-			if(len(jobs) > subprocess_count):
-				for j in jobs: j.join()
-				jobs.clear()
-
-				while not queue.empty():
-					distance, gallerysub = queue.get()
-					distances[gallerysub].append(distance)
+			# Get the process list and add it to the distances 
+			while not queue.empty():
+				distances.append(queue.get())
 	
+	# Same for all unfinished processes
 	for p in jobs: p.join()
 	while not queue.empty():
-		distance, gallerysub = queue.get()
-		distances[gallery_subject].append(distance)
+		distances.append(queue.get())
 
+	# Closing queue
 	queue.close()
 	queue.join_thread()
 	
+	# Finding minimum distance based on precalculated distances
 	for gallery_subject in range(len(distances)):
 		for dist in distances[gallery_subject]:
 
