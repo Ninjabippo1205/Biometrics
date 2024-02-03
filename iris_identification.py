607b1acf2146b4bc3c55540cb854aa37bdd8edd3
Filippo Olimpieri <126257005+Ninjabippo1205@users.noreply.github.com>
2024-02-03 09:20:59 +0100

    added async multiprocessing

--- iris_identification.py
+++ iris_identification.py
@@ -1,70 +1,54 @@
 import cv2
 import iris_recognition as IR
 from scipy.spatial import distance as scipydistance
-import multiprocessing
+from multiprocessing import Pool
+
                 
-def subject_hamming_distance(gallery, gallery_subjects, gallery_subject, path, image_path, probeimage, queue, ):
-	distances = []
+def subject_hamming_distance(gallery, gallery_subjects, gallery_subject, path, image_path, probeimage):
+    distances = []
 
-	for i in range(len(gallery[gallery_subject])):
-		test_path = gallery[gallery_subject][i]
-		if(image_path == test_path): continue
+    for i in range(len(gallery[gallery_subject])):
+        test_path = gallery[gallery_subject][i]
+        if(image_path == test_path): continue
 
-		galleryimage = cv2.imread(f'{path}/{gallery_subjects[gallery_subject]}/{test_path}')
-		galleryimage = IR.getTemplate(galleryimage).ravel()
+        galleryimage = cv2.imread(f'{path}/{gallery_subjects[gallery_subject]}/{test_path}')
+        galleryimage = IR.getTemplate(galleryimage).ravel()
 
-		distances.append(scipydistance.hamming(probeimage, galleryimage))
-	
-	queue.put(distances)
-	return 0 #correct exit code
+        distances.append(scipydistance.hamming(probeimage, galleryimage))
+    
+    return distances  # Return distances instead of putting it in a queue
 
-def image_matching(path, test_subject, probe, gallery, gallery_subjects, threshold, subprocess_count=7):
-	minDistance = float("inf")
-	matched = ''
-	matched_list = {}
 
-	# Getting template for probe
-	probeimage = cv2.imread(f"{path}/{test_subject}/{probe}")
-	probeimage = IR.getTemplate(probeimage).ravel()
+def image_matching(path, test_subject, probe, gallery, gallery_subjects, threshold, subprocess_count=14):
+    minDistance = float("inf")
+    matched = ''
+    matched_list = {}
 
-	# Multiprocessing items
-	jobs = []
-	distances = []
-	queue = multiprocessing.Queue()
+    # Getting template for probe
+    probeimage = cv2.imread(f"{path}/{test_subject}/{probe}")
+    probeimage = IR.getTemplate(probeimage).ravel()
 
-	# Use a process for each subject
-	for gallery_subject in range(len(gallery)):
-		p = multiprocessing.Process(target=subject_hamming_distance, args=(gallery, gallery_subjects, gallery_subject, path, probe, probeimage, queue, ))
-		jobs.append(p)
-		p.start()
+    # Create a pool of processes
+    with Pool(processes=subprocess_count) as pool:
+    # Use a process for each subject
+        results = []
+        for gallery_subject in range(len(gallery)):
+            result = pool.apply_async(subject_hamming_distance, args=(gallery, gallery_subjects, gallery_subject, path, probe, probeimage))
+            results.append(result)
 
-		if len(jobs) > subprocess_count:
-			for j in jobs: j.join()
-			jobs.clear()
+    	# Collect the results as they become available
+        distances = [result.get() for result in results]
 
-			# Get the process list and add it to the distances 
-			while not queue.empty():
-				distances.append(queue.get())
-	
-	# Same for all unfinished processes
-	for p in jobs: p.join()
-	while not queue.empty():
-		distances.append(queue.get())
+    # Finding minimum distance based on precalculated distances
+    for gallery_subject in range(len(distances)):
+        for dist in distances[gallery_subject]:
 
-	# Closing queue
-	queue.close()
-	queue.join_thread()
-	
-	# Finding minimum distance based on precalculated distances
-	for gallery_subject in range(len(distances)):
-		for dist in distances[gallery_subject]:
+            if(dist < minDistance):
+                minDistance = dist
+                matched = gallery_subjects[gallery_subject]
 
-			if(dist < minDistance):
-				minDistance = dist
-				matched = gallery_subjects[gallery_subject]
+            if(dist < threshold):
+                matched_list[gallery_subjects[gallery_subject]] = dist
 
-			if(dist < threshold):
-				matched_list[gallery_subjects[gallery_subject]] = dist
-
-	matched_list = dict(sorted(matched_list.items(), key=lambda item: item[1]))	
-	return [matched, minDistance, matched_list]
+    matched_list = dict(sorted(matched_list.items(), key=lambda item: item[1]))    
+    return [matched, minDistance, matched_list]
--- iris_recognition.py
+++ iris_recognition.py
@@ -234,7 +234,7 @@ def build_filters():
 	filters = []
 	ksize = 31
 	for theta in np.arange(0, np.pi, np.pi/16):
-		params = {'ksize' : (ksize, ksize), 'sigma' : 1.0, 'theta' : theta, 'lambd' : 15.0, 'gamma' : 0.02, 'psi' : 0, 'ktype' : cv2.CV_32F}
+		params = {'ksize' : (ksize, ksize), 'sigma' : 1.0, 'theta' : theta, 'lambd' : 15.0, 'gamma' : 2, 'psi' : 0, 'ktype' : cv2.CV_32F}
 		kern = cv2.getGaborKernel(**params)
 		kern /= 1.5 * kern.sum()
 		filters.append((kern, params))
