21f8bbca43271a73a7031657d130ec1306007496
Filippo Guerra <filippoguerra2001@gmail.com>
2024-02-06 15:41:31 +0100

    Moved dataset and applied face recognition to support it

--- faceRecognition-2/face_testing.py
+++ faceRecognition-2/face_testing.py
@@ -1,15 +1,32 @@
 import cv2, time, os
-import json, requests
+import json, requests, argparse
 import face_training
 
 def main():
+    parser = argparse.ArgumentParser(
+		prog="FaceTesting",
+		description="This program handles testing of a trained model based on a given face recognition dataset",
+		epilog="The dataset must be created as such: \"dataset/xxx/xxx_y.bmp\" where \'x\' is the subject's number and \'y\' the numbered photo."
+	)
+    
+    parser.add_argument('-p', '--path', dest="path", type=str, default="CASIA-FaceV5", help="Folder where iris images are saved")
+    parser.add_argument('-td', '--test-dataset', dest="testset", default="test.json", help="File that holds the test portion of the model")
+    arguments = parser.parse_args()
+
+    if not os.path.exists(arguments.path):
+        print("Path folder not found.")
+        exit(-1)
+    
+    if not os.path.exists(arguments.testset):
+        print("Test dataset representation could not be found")
+        exit(-1)
+
     if not os.path.exists('Trainer.yml'):
         print("Please proceed to train the model first")
         exit(-1)
     
-    if not os.path.exists('dataset.json'):
-        with open('dataset.json') as f:
-            dataset = json.load(f) 
+    with open(arguments.testset) as f:
+        testset = json.load(f) 
 
     if not os.path.exists("haarcascade_frontalface_default.xml"):
         content = requests.get("https://raw.githubusercontent.com/opencv/opencv/master/data/haarcascades/haarcascade_frontalface_default.xml")
@@ -19,31 +36,29 @@ def main():
     recognizer = cv2.face.LBPHFaceRecognizer_create()
     recognizer.read("Trainer.yml")
 
+    TP = 0; TN = 0; FP = 0; FN = 0
+
+    for subject in testset:
+        for image in testset[subject]:
+            faceImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            faces = face_training.detectMultiScale(faceImage, 1.3, 5)
+
+            for (x,y,w,h) in faces:
+                serial, conf = recognizer.predict(faceImage[y:y+h, x:x+w])
+
+                print(serial, conf)
 
 
-while True:
-    gray=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_training.detectMultiScale(gray, 1.3, 5)
-    for (x,y,w,h) in faces:
-        serial, conf = recognizer.predict(gray[y:y+h, x:x+w])
-        if conf>60:
-            cv2.rectangle(frame, (x,y), (x+w, y+h), (0,0,255), 1)
-            cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
-            cv2.rectangle(frame,(x,y-40),(x+w,y),(255,0,0),-1)
-            cv2.putText(frame, trainingdemo.names[serial], (x, y-10),cv2.FONT_HERSHEY_SIMPLEX,0.8,(255,255,255),2)
-        else:
-            cv2.rectangle(frame, (x,y), (x+w, y+h), (0,0,255), 1)
-            cv2.rectangle(frame,(x,y),(x+w,y+h),(50,50,255),2)
-            cv2.rectangle(frame,(x,y-40),(x+w,y),(50,50,255),-1)
-            cv2.putText(frame, "Unknown", (x, y-10),cv2.FONT_HERSHEY_SIMPLEX,0.8,(255,255,255),2)
-    frame=cv2.resize(frame, (640, 480))
-    
-    cv2.imshow("Frame",frame)
-    k=cv2.waitKey(1)
-    
-    if k==ord('o') and conf>50:
-        time.sleep(10)
-    if k==ord("q"):
-        break
-
-cv2.destroyAllWindows()
+#                if conf>60:
+#                    cv2.rectangle(frame, (x,y), (x+w, y+h), (0,0,255), 1)
+#                    cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+#                    cv2.rectangle(frame,(x,y-40),(x+w,y),(255,0,0),-1)
+#                    cv2.putText(frame, trainingdemo.names[serial], (x, y-10),cv2.FONT_HERSHEY_SIMPLEX,0.8,(255,255,255),2)
+#                else:
+#                    cv2.rectangle(frame, (x,y), (x+w, y+h), (0,0,255), 1)
+#                    cv2.rectangle(frame,(x,y),(x+w,y+h),(50,50,255),2)
+#                    cv2.rectangle(frame,(x,y-40),(x+w,y),(50,50,255),-1)
+#                    cv2.putText(frame, "Unknown", (x, y-10),cv2.FONT_HERSHEY_SIMPLEX,0.8,(255,255,255),2)
+                
+if __name__ == '__main__':
+    main()
--- faceRecognition-2/face_training.py
+++ faceRecognition-2/face_training.py
@@ -1,46 +1,81 @@
 import cv2, os, json
 import numpy as np
+import argparse
 
-def createDataset(path):
-    dataset = {}
-
+def createDataset(path, trainsize):
     if not os.path.exists(path):
         print("Dataset could not be loaded, please check folder location")
         exit(-1)
-    
+
+    if os.path.exists('train.json'):
+        file = open('train.json', 'r')
+        dataset = json.load(file)
+        file.close()
+        return dataset
+
+    train = {}
+    test = {}
+
     for subject in os.listdir(path):
-        dataset[subject] = os.listdir(f'{path}/{subject}')
+        files = os.listdir(f'{path}/{subject}')
+        train[subject] = files[:int(len(files)*trainsize)]
+        test[subject] = files[int(len(files)*trainsize):]
     
-    file = open('dataset.json', 'w')
-    json.dump(dataset, file)
+    file = open('train.json', 'w')
+    json.dump(train, file)
     file.close()
 
-    return dataset
+    file = open('test.json', 'w')
+    json.dump(test, file)
+    file.close()
 
-def getImages(dataset, path):
+    return train, test
+
+def getImages(trainset, path):
     ids = []
     faces = []
 
-    for subject in dataset:
-        for image in dataset[subject]:
-            faceImage = np.array(cv2.imread(f'{path}/{subject}/{image}'))
+    for subject in trainset:
+        for image in trainset[subject]:
+            faceImage = np.array(cv2.imread(f'{path}/{subject}/{image}', cv2.IMREAD_GRAYSCALE))
             faces.append(faceImage)
-            ids.append(subject)
-            cv2.imshow("Training", faceImage)
-            cv2.waitKey(1)
+            ids.append(int(subject))
+            #cv2.imshow("Training", faceImage)
+            #cv2.waitKey(1)
     return ids, faces
 
 def main():
-    recognizer = cv2.face.LBPHFaceRecognizer_create()
-    path="faceRecognition/"
+    parser = argparse.ArgumentParser(
+		prog="FaceTraining",
+		description="This program handles training a model based on a given face recognition dataset",
+		epilog="The dataset must be created as such: \"dataset/xxx/xxx_y.bmp\" where \'x\' is the subject's number and \'y\' the numbered photo."
+	)
+    
+    parser.add_argument('-p', '--path', dest="path", type=str, default="CASIA-FaceV5", help="Folder where iris images are saved")
+    parser.add_argument('-ts', '--train-size', dest="tsize", default="0.8", help="Amount of data used for training")
+    arguments = parser.parse_args()
 
-    dataset = createDataset(path)
-    IDs, faceData = getImages(dataset)
+    if not os.path.exists(arguments.path):
+        print("Path folder not found.")
+        exit(-1)
+    
+    try:
+        size = float(arguments.tsize)
+        if size < 0 or size >= 1: raise ValueError
+    except ValueError:
+        print("Argument size not valid. Please provide a decimal number between 0 and 0.9")
+        exit(-1)
+
+    recognizer = cv2.face.LBPHFaceRecognizer_create()
+
+    train, test = createDataset(arguments.path, size)
+    IDs, faceData = getImages(train, arguments.path)
 
     recognizer.train(faceData, np.array(IDs))
-    recognizer.write('Trainer.yml')
-    cv2.destroyAllWindows()
     print("-------------- Training Complete ------------")
+    recognizer.write('Trainer.yml')
+    print("------------- Saved Trained Model -----------")
+    cv2.destroyAllWindows()
     return
 
 if __name__ == "__main__":
